b72473e7347ccb9f31c21ed35a1bec1c56b75259
Dimitri Vasilev <dimitrovasilev@gmail.com>
2021-10-24 17:30:58 -0700

    Project start

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-import { hot } from "react-hot-loader";
+import * as React from 'react';
+import {hot} from 'react-hot-loader';
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
+const reactLogo = require('./../assets/img/react_logo.svg');
+import './../assets/scss/App.scss';
 
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
+        <h1>Hello there!</h1>
         <p>Foo to the barz</p>
         <img src={reactLogo.default} height="480" />
       </div>
